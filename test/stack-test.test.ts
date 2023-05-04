fb5283d36611c52a754b6f37b7beb31663c6396f
Konstantyn Niechniedowicz <konstantyn.niechniedowicz@gmail.com>
2023-05-04 09:15:10 +0200

    Initial commit

---
+++ test/stack-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StackTest from '../lib/stack-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stack-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StackTest.StackTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
